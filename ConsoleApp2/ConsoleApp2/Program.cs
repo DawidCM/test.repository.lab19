7e2a223288e596ae681b99f06a1aa489c3237a08
Dawid Korba <P2577974@my365.dmu.ac.uk>
2022-02-09 11:43:20 +0000

    Dawid Changes

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dawid.Changes");
+            Console.ReadLine();
         }
     }
 }
